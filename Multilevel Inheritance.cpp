c5d672346d820518311262843a1eede7b48b1412
Sairaj Shirole <121667877+sairaj2003@users.noreply.github.com>
2023-10-21 01:18:03 +0530

    Add files via upload

---
+++ Multilevel Inheritance.cpp
@@ -0,0 +1,47 @@
+//Multi-level Inheritance:
+
+#include <iostream>
+using namespace std;
+
+class base{
+	public:
+		void display()
+		{
+			cout<<"Hey i am from base class.."<<endl;
+		}
+};
+
+class derive1:public base{
+	public:
+		void display()
+		{
+			cout<<"Hey i am from derive1 class..."<<endl;
+		}
+};
+
+class derive2:public derive1{
+	public:
+		void display()
+		{
+			cout<<"Hey i am from derive2 class..."<<endl;
+		}
+};
+
+int main() 
+{
+	base obj1;
+	obj1.display();
+	cout<<"--------------------------------------------------------------\n";
+	
+	derive1 obj2;
+	obj2.display();
+	obj2.base::display();
+	cout<<"--------------------------------------------------------------\n";
+	
+	derive2 obj3;
+	obj3.display();
+	obj3.base::display();
+	obj3.derive1::display();
+	
+	return 0;
+}
