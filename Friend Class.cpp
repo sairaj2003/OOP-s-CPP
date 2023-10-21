8bfc3d91ecab6c1eb8e437adf03f116ca91e8024
Sairaj Shirole <121667877+sairaj2003@users.noreply.github.com>
2023-10-21 21:44:54 +0530

    Friend Class.cpp

--- Friend Class.cpp
+++ Friend Class.cpp
@@ -11,27 +11,28 @@ class A
 	public:
 		void show()
 		{
-			cout<<"a"<<a<<" "<<"b"<<b<<endl;	
+			cout<<"a:"<<a<<" "<<"b:"<<b<<endl;	
 		}
 		
-		friend class B;	
+		friend class B;	   //declare friend class
 };
 
 class B
 {
 	public:
-		void add(A ref)
+		void add(A ref)         // ref is an object of class A that is passed as a parameter
 		{
 			int sum=ref.a+ref.b;
 			cout<<"Addition of a and b is:"<<sum;
 		}	
 };
+
 int main() 
 {
 	A obj1;
 	obj1.show();
 	
 	B obj2;
-	obj2.add(obj1);	
+	obj2.add(obj1);	 //In class B, the add function should take an object of class A as a parameter because it needs access to the private members 'a' and 'b' of class A.
 	return 0;
 }
